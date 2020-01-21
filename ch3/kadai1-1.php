8a020647fead6d5df115d45a99316f40b75da8e9
Yasuhiro <felnand9torres@gmail.com>
2020-01-21 13:29:07 +0000

    ch3課題提出

---
+++ ch3/kadai1-1.php
@@ -0,0 +1,9 @@
+<?php
+    $name="Yasuhiro";
+    if($name=="Yasuhiro"){
+        echo "私はあなたの名前です";
+    }else{
+        echo "あなたの名前ではありません";
+    }
+
+?>
---
+++ ch3/kadai1-3.php
@@ -0,0 +1,6 @@
+<?php
+    $fruits=array("メロン","桃","パイナップル","りんご","イチゴ",);
+    foreach($fruits as $fruit){
+        echo $fruit;
+    }
+?>
